7daf52c66647839f153fe0836a71a9383572d076
dani-sam <samdani295@gmail.com>
2024-02-03 21:14:29 +0530

    day1

---
+++ class1/flaskTutorial/tutorial1.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect , url_for
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return"Hello! this the main page <h1>HELLO<h1>"
+
+@app.route("/<name>")
+def name(name):
+	return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+	return redirect(url_for("home"))
+
+if __name__ == "__main__":
+	app.run()
